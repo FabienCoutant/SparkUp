c355ddc285a75c7c116e2ffddeb95a6542427e9e
RayXpub <doxdev.reargues@aleeas.com>
2021-07-07 22:12:24 +0200

    header build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useEagerConnect, useInactiveListener } from './hooks/web3';
+import Header from './components/UI/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const triedEager = useEagerConnect();
   useInactiveListener(!triedEager);
 
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
